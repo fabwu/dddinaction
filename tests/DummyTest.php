e4323ab535885cb71f91ec91024b2306d7d3fa53
Fabian Wüthrich <me@fabwu.ch>
2018-03-01 22:43:16 +0700

    Add phpunit

---
+++ tests/DummyTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Tests;
+
+
+use PHPUnit\Framework\TestCase;
+
+class DummyTest extends TestCase
+{
+    public function testFalse()
+    {
+        $this->assertFalse(false);
+    }
+}
