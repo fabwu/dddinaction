a46ffaa5440e6ebd9e2d6278333cafa118eb20e0
Fabian Wüthrich <me@fabwu.ch>
2018-06-25 15:22:32 +0700

    Add load cash to atm

--- src/Domain/Management/HeadOffice.php
+++ src/Domain/Management/HeadOffice.php
@@ -4,6 +4,7 @@
 namespace App\Domain\Management;
 
 
+use App\Domain\Atm\Atm;
 use App\Domain\Common\AggregateRoot;
 use App\Domain\Common\Utility;
 use App\Domain\SharedKernel\Money;
@@ -33,6 +34,12 @@ class HeadOffice extends AggregateRoot
     {
         $money      = $snackMachine->unloadMoney();
         $this->cash = $this->cash->add($money);
+    }
+
+    public function loadCashToAtm(Atm $atm): void
+    {
+        $atm->loadMoney($this->cash);
+        $this->cash = Money::None();
     }
 
     public function getBalance(): float
--- tests/unit/HeadOfficeTest.php
+++ tests/unit/HeadOfficeTest.php
@@ -2,6 +2,7 @@
 
 namespace App\Tests\unit;
 
+use App\Domain\Atm\Atm;
 use App\Domain\Management\HeadOffice;
 use App\Domain\SharedKernel\Money;
 use App\Domain\SnackMachine\SnackMachine;
@@ -9,14 +10,19 @@ use PHPUnit\Framework\TestCase;
 
 class HeadOfficeTest extends TestCase
 {
-    public function test_unload_cash_from_snack_machine(): void
+    public function test_transfer_cash_from_snack_machine_to_atm(): void
     {
         $headOffice   = new HeadOffice();
         $snackMachine = new SnackMachine();
         $snackMachine->loadMoney(Money::Dollar());
+        $atm = new Atm();
 
         $headOffice->unloadCashFromSnackMachine($snackMachine);
+        $headOffice->loadCashToAtm($atm);
 
-        $this->assertEquals(1, $headOffice->getCash()->getAmount());
+
+        $this->assertEquals(0, $headOffice->getCash()->getAmount());
+        $this->assertEquals(0, $snackMachine->getMoneyInside()->getAmount());
+        $this->assertEquals(1, $atm->getMoneyInside()->getAmount());
     }
 }
